4510de892cbdb03a0917351381f643de755bc99e
Chris Kim <contact@chriskkim.com>
2025-05-02 12:56:27 +0000

    WIP preparation

---
+++ app/wip/helloworld/page.tsx
@@ -0,0 +1,10 @@
+export default function Page(){
+
+    return <>
+        <div className="container my-5">
+            Hello from Chris K 🖐️
+        </div>
+    </>
+
+
+}
---
+++ app/wip/services/page.tsx
@@ -0,0 +1,9 @@
+export default function Page(){
+
+    return <>
+        <div className="container my-5">
+            - SERVICES PAGE -
+        </div>
+    </>
+
+}
