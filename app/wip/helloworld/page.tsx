6ac89351633866b6ef35a8f39e43f2073b336462
ow415263 <60719276+ow415263@users.noreply.github.com>
2025-05-06 17:02:52 +0000

    Add Hello, World! to helloworld page.

--- app/wip/helloworld/page.tsx
+++ app/wip/helloworld/page.tsx
@@ -2,7 +2,7 @@ export default function Page(){
 
     return <>
         <div className="container my-5">
-            <h1>Hello, World!!</h1>
+            <h1>Hello, World!</h1>
         </div>
     </>
 
